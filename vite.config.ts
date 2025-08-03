a8422b0eebe49c95e8bcbbd7ca2d70965a55d081
tmih06 <thminhdeptrai@gmail.com>
2025-08-03 22:29:53 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -46,14 +46,6 @@ export default defineConfig({
     })
   ],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true
-      },
-      format: {
-        comments: false
-      }
-    }
+    minify: false  // Disabled for debugging
   }
 })
